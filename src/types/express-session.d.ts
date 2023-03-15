38554e73ad01178af407aea4337a4ebc727edb3b
Katsuki Oike <katsuki.oike@gmail.com>
2023-03-15 10:26:10 -0500

    Update the Session interface

--- src/types/express-session.d.ts
+++ src/types/express-session.d.ts
@@ -2,9 +2,12 @@ import 'express-session';
 
 declare module 'express-session' {
   export interface Session {
-    clearSession(): Promise<void>; // DO NOT MODIFY THIS!
+    clearSession(): Promise<void>;
 
-    // NOTES: Add your app's custom session properties here:
-   
+    authenticatedUser: {
+      userId: string;
+      username: string;
+    };
+    isLoggedIn: boolean;
   }
 }
