c81adc7a6c9d1c2468e87360502775e79e2a579e
Katsuki Oike <katsuki.oike@gmail.com>
2023-03-18 16:03:33 -0500

    Add getUserByUsername and addNewUser function

---
+++ src/models/UserModel.ts
@@ -0,0 +1,23 @@
+import { AppDataSource } from '../dataSource';
+import { User } from '../entities/User';
+
+const userRepository = AppDataSource.getRepository(User);
+
+async function getUserByUsername(username: string): Promise<User | null> {
+  // TODO: Get the user by where the username matches the parameter
+  // This should also retrieve the `links` relation
+  return await userRepository.findOne({ where: { username } });
+}
+
+async function addNewUser(username: string, passwordHash: string): Promise<User | null> {
+  // TODO: Add the new user to the database
+  let newUser = new User();
+  newUser.username = username;
+  newUser.passwordHash = passwordHash;
+
+  newUser = await userRepository.save(newUser);
+
+  return newUser;
+}
+
+export { getUserByUsername, addNewUser };
