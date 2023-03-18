1a3fb9dd50921110a0dc8054711d26c960cfe78e
Katsuki Oike <katsuki.oike@gmail.com>
2023-03-18 16:36:29 -0500

    Implement the session management

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
 import './config'; // Load environment variables
 import 'express-async-errors'; // Enable default error handling for async errors
+import express, { Express } from 'express';
+
+import session from 'express-session';
+import connectSqlite3 from 'connect-sqlite3';
+import { registerUser, logIn } from './controllers/UserController';
+
+const app: Express = express();
+const { PORT, COOKIE_SECRET } = process.env;
+
+const SQLiteStore = connectSqlite3(session);
+
+app.use(
+  session({
+    store: new SQLiteStore({ db: 'session.sqlite' }),
+    secret: COOKIE_SECRET,
+    cookie: { maxAge: 8 * 60 * 60 * 1000 },
+    name: 'session',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(express.json());
+
+app.post('api/users', registerUser); // Create an account
+app.post('api/login', logIn); // Log in to an account
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
+});
