b20bef06b337b8d4c54db2395bb8fe11cc5d0e1b
Katsuki Oike <katsuki.oike@gmail.com>
2023-04-03 21:05:22 -0500

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
 import './config'; // Load environment variables
 import 'express-async-errors'; // Enable default error handling for async errors
+
 import express, { Express } from 'express';
+
 import session from 'express-session';
 import connectSqlite3 from 'connect-sqlite3';
 import { registerUser, logIn } from './controllers/UserController';
-import {
-  shortenUrl,
-  getOriginalUrl,
-  getLinkForProAdmin,
-  deleteLink,
-} from './controllers/LinkController';
+import { shortenUrl, visitLink, getLinks, removeLink } from './controllers/LinkController';
 
 const app: Express = express();
 const { PORT, COOKIE_SECRET } = process.env;
+
 const SQLiteStore = connectSqlite3(session);
 
 app.use(
   session({
-    store: new SQLiteStore({ db: 'session.sqlite' }),
+    store: new SQLiteStore({ db: 'sessions.sqlite' }),
     secret: COOKIE_SECRET,
-    cookie: { maxAge: 8 * 60 * 60 * 1000 },
+    cookie: { maxAge: 8 * 60 * 60 * 1000 }, // 8 hours
     name: 'session',
     resave: false,
     saveUninitialized: false,
@@ -30,10 +28,11 @@ app.use(express.json());
 
 app.post('/api/users', registerUser); // Create an account
 app.post('/api/login', logIn); // Log in to an account
-app.post('/api/shortlink', shortenUrl); // Create a shortened link
-app.get('/api/:targetLinkId', getOriginalUrl);
-app.post('/api/users/:userId/links', getLinkForProAdmin);
-app.delete('/api/users/:userId/links/:linkId', deleteLink);
+
+app.post('/api/links', shortenUrl);
+app.get('/:targetLinkId', visitLink);
+app.get('/api/users/:targetUserId/links', getLinks);
+app.delete('/api/users/:targetUserId/links/:targetLinkId', removeLink);
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
