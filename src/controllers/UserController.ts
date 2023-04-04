8ac41715460475b04e381c2c4e0437e049c62d4c
Katsuki Oike <katsuki.oike@gmail.com>
2023-04-03 20:38:30 -0500

    Update some functions in UserController.ts

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,17 +1,14 @@
 import { Request, Response } from 'express';
 import argon2 from 'argon2';
-import { getUserByUsername, addNewUser } from '../models/UserModel';
+import { addNewUser, getUserByUsername } from '../models/UserModel';
 import { parseDatabaseError } from '../utils/db-utils';
 
 async function registerUser(req: Request, res: Response): Promise<void> {
-  // TODO: Implement the registration code
-  // Make sure to check if the user with the given username exists before attempting to add the account
-  // Make sure to hash the password before adding it to the database
-  // Wrap the call to `addNewUser` in a try/catch like in the sample code
   const { username, password } = req.body as AuthRequest;
+
   const user = await getUserByUsername(username);
   if (user) {
-    res.sendStatus(403); // 403 Forbidden - username exists
+    res.sendStatus(409);
     return;
   }
 
@@ -19,8 +16,8 @@ async function registerUser(req: Request, res: Response): Promise<void> {
 
   try {
     const newUser = await addNewUser(username, passwordHash);
-    console.log(newUser);
-    res.sendStatus(201);
+
+    res.status(201).json(newUser);
   } catch (err) {
     console.error(err);
     const databaseErrorMessage = parseDatabaseError(err);
@@ -30,24 +27,25 @@ async function registerUser(req: Request, res: Response): Promise<void> {
 
 async function logIn(req: Request, res: Response): Promise<void> {
   const { username, password } = req.body as AuthRequest;
+
   const user = await getUserByUsername(username);
   if (!user) {
-    res.sendStatus(404); // 404 Not Found - username doesn't exist
+    res.sendStatus(404); // 404 Not Found - email doesn't exist
     return;
   }
 
   const { passwordHash } = user;
   if (!(await argon2.verify(passwordHash, password))) {
-    res.sendStatus(404); // 404 Not Found - user with username/pwd doesn't exist
+    res.sendStatus(404); // 404 Not Found - user with email/pass doesn't exist
     return;
   }
 
   await req.session.clearSession();
   req.session.authenticatedUser = {
     userId: user.userId,
+    username: user.username,
     isPro: user.isPro,
     isAdmin: user.isAdmin,
-    username: user.username,
   };
   req.session.isLoggedIn = true;
 
